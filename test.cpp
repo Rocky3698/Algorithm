31baca26cab1edbda383534fda0a13dbc44d4287
Rocky Chowdhury <rocky20809@gmail.com>
2023-07-30 03:56:27 +0600

    x

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cout<<"Working";
+
+    return 0;
+}
